475bd9b1f1d6d709dc7851378cce719787cf7849
Nimur Rahman Sharif Tasrik <126714137+shariftasrik@users.noreply.github.com>
2024-07-05 04:47:32 +0600

    Carousel and Features section added

--- edu-academy/src/Screen/sidebar.js
+++ edu-academy/src/Screen/sidebar.js
@@ -3,12 +3,12 @@ import logo from '../Assets/Images/Logos/Logo-1.png';
 import closeIcon from '../Assets/svg/x-circle.svg';
 import '../css/sidebar.css';
 
-function Sidebar({ toggleSidebar }) {
+function Sidebar({ visible, toggleSidebar }) {
   return (
-    <div className="sidebar fixed">
+    <div className={`sidebar ${visible ? 'visible' : ''}`}>
       <img src={closeIcon} className="close-icon" alt="Close" onClick={toggleSidebar} />
-      <a href="#" className="logo bl">
-        <img src={logo} className="bl" alt="Logo" />
+      <a href="#" className="logo">
+        <img src={logo} className="logo-image" alt="Logo" />
       </a>
     </div>
   );
